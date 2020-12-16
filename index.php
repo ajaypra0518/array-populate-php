c590ee0b0bc48559c17d230dd526958a00425a2e
Ajay Prajapati <74779607+ajaypra0518@users.noreply.github.com>
2020-12-16 12:09:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -75,9 +75,7 @@ $products = array(
                 )
   );
 
-//   echo "<pre>";
-//  print_r($products);
-//   echo "</pre>";
+
 ?>
 
 <!DOCTYPE html>
@@ -86,9 +84,20 @@ $products = array(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    .center{
+        position:absolute;
+        height:100vh;
+        top:50%;
+        left:50%;
+        transform:translate(-50%,-50%);
+       
+    }
+    </style>
 </head>
-<body>
+<body class="center">
   <?php
+  echo "<h3>PART 1</h3>";
     echo "<table border='1'>
                 <tr>
                 <th>Category</th>
@@ -114,7 +123,7 @@ $products = array(
         }      
      echo "</table>";
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////     
 
      echo "<table border='1'>
                 <tr>
@@ -125,6 +134,7 @@ $products = array(
                 <th>Brand</th>
                 </tr>";
     echo "<br> <br>";
+    echo "<h3>PART 2</h3>";
 
      foreach($products as $key=>$value){
         foreach($value as $key1=>$value1){ 
@@ -143,36 +153,32 @@ $products = array(
     }  
     echo "</table>"; 
     
-   
-    
-    
-
+    echo "<h3>PART 3</h3>";
     foreach($products as $key=>$value){
         foreach($value as $key1=>$value1){ 
             foreach($value1 as $key2=>$value2){         
                 foreach($value2 as $key3=>$value3){
-                    if($value3=="Samsung"){
-                       
-                        echo "<br><br>";
+                    if($value3=="Samsung"){                      
                         echo "<span>Product ID:</span>".$value2['id'];
                         echo "<br>";
                         echo "<span>Product Name:</span>".$value2['name'];
                         echo "<br>";
                         echo "<span>Sub Category:</span>".$key1;
                         echo "<br>";
-                        echo "<span>Category:</span>".$key;
-                        
-
-                      
-                    }                      
-                                         
-                }
-               
-            }
-             
+                        echo "<span>Category:</span>".$key; 
+                        echo "<br><br>";                     
+                    }                                                              
+                }               
+            }             
         }
-    } 
+    }
+    
+    ////////////////////////////Deletion Start//////////////////////////////
+
     echo "<br><br>";
+    echo "<h3>PART 4</h3>";
+   
+    
     echo "<table border='1'>
     <tr>
     <th>Category</th>
@@ -182,51 +188,28 @@ $products = array(
     <th>Brand</th>
     </tr>";
 
-    foreach($products as $key=>$value){
-        foreach($value as $key1=>$value1){ 
-            foreach($value1 as $key2=>$value2){
-                echo "<tr>";
-                echo "<td>".$key."</td>";
-                echo "<td>".$key1."</d>";               
-                foreach($value2 as $key3=>$value3){
-                    if($value3=="PR002"){
-                        $value2['name']="BIG-555";
-                    }                      
-                    // echo "<td>".$value3."</td>";
-                    echo "<td>".$value2[$key3]."</td>";                     
-                }
-               
-            }
-             
-        }
-    }  
-    echo "<br><br>";
-    echo "</table>"; 
 
 
-
-    echo "<table border='1'>
-    <tr>
-    <th>Category</th>
-    <th>Sub Category</th>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Brand</th>
-    </tr>";
-
+    // foreach($products as $key=>$value){
+    //     foreach($value as $key1=>$value1){ 
+    //         foreach($value1 as $key2=>$value2){         
+    //             foreach($value2 as $key3=>$value3){
+    //                 if($value3=="PR003"){                      
+    //                     unset($products['Electronics']['Television'][2]);                
+    //                 }                                                              
+    //             }               
+    //         }             
+    //     }
+    // }
+    unset($products['Electronics']['Television'][2]);  
 
 foreach($products as $key=>$value){
-    foreach($value as $key1=>$value1){      
-        foreach($value1 as $key2=>$value2){
-        if($value2['id']=="PR003"){
-            // unset($value1['$key2']);
-            unset[$products['']]
-    
-        }
+foreach($value as $key1=>$value1){            
+    foreach($value1 as $key2=>$value2){
         echo "<tr>";
         echo "<td>".$key."</td>";
         echo "<td>".$key1."</d>";                  
-        foreach($value2 as $key3=>$value3){                  
+        foreach($value2 as $key3=>$value3){                       
             echo "<td>".$value3."</td>";                      
         }
         echo "</tr> ";
@@ -234,7 +217,39 @@ foreach($products as $key=>$value){
 }
 }      
 echo "</table>";
+  ////////////////////////////Deletion END//////////////////////////////
 
+
+////////////////////////////Updation Start//////////////////////////////
+    echo "<br>";
+   
+    echo "<h3>PART 5</h3>";
+    echo "<table border='1'>
+    <tr>
+    <th>Category</th>
+    <th>Sub Category</th>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Brand</th>
+    </tr>";
+
+$products['Electronics']['Television'][1]['name']="BIG-555";
+foreach($products as $key=>$value){
+foreach($value as $key1=>$value1){            
+    foreach($value1 as $key2=>$value2){
+        echo "<tr>";
+        echo "<td>".$key."</td>";
+        echo "<td>".$key1."</d>";                  
+        foreach($value2 as $key3=>$value3){                       
+            echo "<td>".$value3."</td>";                    
+        }
+        echo "</tr> ";
+    }
+}
+}      
+echo "</table>";
+
+////////////////////////////Updation END//////////////////////////////
 
   ?>
     
